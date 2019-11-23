8c9c7ddd657acf10b4161d2399dc2f0c8b8d4b77
Carlos Valverde <studio@carlos-valverde.com>
2019-11-23 21:52:57 +0100

    Update // Version 0.1.0

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link @click="$refs.fullpage.api.moveTo(1,0)" to="/">Zarata Fest XIII</router-link> |
+      <router-link to="/">Zarata Fest XIII</router-link> |
       <router-link to="/contacto">Contacto</router-link> |
       <router-link to="/acerca-de-zarata-fest">Acerca de Zarata Fest</router-link>
       
